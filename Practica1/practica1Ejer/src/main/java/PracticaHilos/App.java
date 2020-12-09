c34e06734efbf62793777dfc79850e7bc9e18879
ElManu3le <62712661+ElManu3le@users.noreply.github.com>
2020-12-09 18:40:34 +0100

    ProyectStart

---
+++ Practica1/practica1Ejer/src/main/java/PracticaHilos/App.java
@@ -0,0 +1,18 @@
+package PracticaHilos;
+
+
+public class App implements Runnable  //Clase que implementa la interfaz Runnable
+{
+   /*  public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    } */ 
+
+    @Override
+    public void run() {
+       for (int i = 0; i < 100; i++) {
+           
+       }
+
+    }
+}
