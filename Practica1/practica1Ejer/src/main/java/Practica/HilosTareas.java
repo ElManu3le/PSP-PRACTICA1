23366ebb4fa76c3b5b23377bed58002b91900958
ElManu3le <62712661+ElManu3le@users.noreply.github.com>
2020-12-15 15:53:56 +0100

    Fin practica 1

--- Practica1/practica1Ejer/src/main/java/Practica/HilosTareas.java
+++ Practica1/practica1Ejer/src/main/java/Practica/HilosTareas.java
@@ -10,8 +10,8 @@ public class HilosTareas implements Runnable {
 
             contador++;
 
-            int miliseconds = (int) (Math.random() * 5 + 1); // Es necesario poner el (int) antes de
-                                                             // Math.ramdom() sino no tira
+            int miliseconds = (int) (Math.random() * 5 + 1); // Es necesario poner el (int) antes de Math.ramdom() sino no tira 
+                            //Introduciendo el int, marcamos a que el MathRandom solo haga con int y no con double, como lo hace de manera predefinida. 
 
             System.out.println("Soy el hilo hijo " + contador + " y me pongo a dormir " + miliseconds + " segundos");
 
