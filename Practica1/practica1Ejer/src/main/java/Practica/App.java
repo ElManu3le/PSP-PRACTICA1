e7a9d70caabb5a2d92eccbde964a653080df1562
ElManu3le <msancho620@yahoo.es>
2021-05-11 17:23:38 +0200

    jeñe

--- Practica1/practica1Ejer/src/main/java/Practica/App.java
+++ Practica1/practica1Ejer/src/main/java/Practica/App.java
@@ -3,12 +3,14 @@ package Practica;
 public class App
 {
     public static void main(String[] args) {
-        Hilo[] Hilo = new Hilo[100];
+
+        /*Hilo[] Hilo = new Hilo[100];
         for (Hilo listahilos : Hilo) {
             listahilos = new Hilo(1);
         }
 
-        listahilos.start();
+        listahilos.start();*/
+        
 
     }
 
--- Practica1/practica1Ejer/src/main/java/Practica/Cartera.java
+++ Practica1/practica1Ejer/src/main/java/Practica/Cartera.java
@@ -7,11 +7,13 @@ public class Cartera{
         this.money = money;
     }
 
+    /** Este metodo se encarga de ir añadiendo dinero */
     synchronized void addmoney(double cuantity){
         System.out.println("Dinero depositado: " + cuantity + " total de: " + (money += cuantity));
 
     }
 
+    /**Metodo usado para ir disminuyendo la cantidad de dinero */
     synchronized void lessmoney(double cuantity){
         System.out.println("Dinero extraido: " + cuantity + " total de: " + (money -= cuantity));
 
